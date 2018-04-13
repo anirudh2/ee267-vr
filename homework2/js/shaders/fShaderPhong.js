9f3900e917e667eb1814a60428402c99e21a6fa2
Hayato Ikoma <hikoma@stanford.edu>
2018-04-13 09:53:33 -0700

    Homework 2 starter code

---
+++ homework2/js/shaders/fShaderPhong.js
@@ -0,0 +1,76 @@
+/**
+ * @file Phong fragment shader
+ *
+ * @copyright The Board of Trustees of the Leland Stanford Junior University
+ * @version 2018/03/28
+ */
+
+/* TODO (2.2.2) */
+
+var shaderID = "fShaderPhong";
+
+var shader = document.createTextNode( `
+/**
+ * WebGL doesn't set any default precision for fragment shaders.
+ * Precision for vertex shader is set to "highp" as default.
+ * Do not use "lowp". Some mobile browsers don't support it.
+ */
+precision mediump float;
+
+varying vec3 normalCam; // Normal in view coordinate
+varying vec3 fragPosCam; // Fragment position in view cooridnate
+
+uniform mat4 viewMat;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+uniform Material material;
+
+uniform vec3 attenuation;
+
+uniform vec3 ambientLightColor;
+
+
+/***
+ * NUM_POINT_LIGHTS is replaced to the number of point lights by the
+ * replaceNumLights() function in teapot.js before the shader is compiled.
+ */
+#if NUM_POINT_LIGHTS > 0
+
+	struct PointLight {
+		vec3 position;
+		vec3 color;
+	};
+
+	uniform PointLight pointLights[ NUM_POINT_LIGHTS ];
+
+#endif
+
+
+void main() {
+
+	// Compute ambient reflection
+	vec3 ambientReflection = material.ambient * ambientLightColor;
+
+	vec3 fColor = ambientReflection;
+
+	gl_FragColor = vec4( fColor, 1.0 );
+
+}
+` );
+
+
+var shaderNode = document.createElement( "script" );
+
+shaderNode.id = shaderID;
+
+shaderNode.setAttribute( "type", "x-shader/x-fragment" );
+
+shaderNode.appendChild( shader );
+
+document.body.appendChild( shaderNode );
---
+++ homework2/js/shaders/vShaderGouraud.js
@@ -0,0 +1,79 @@
+/**
+ * @file Gouraud vertex shader
+ *
+ * @copyright The Board of Trustees of the Leland Stanford Junior University
+ * @version 2018/03/28
+ */
+
+/* TODO (2.1.2) and (2.1.3) */
+
+var shaderID = "vShaderGouraud";
+
+var shader = document.createTextNode( `
+/**
+ * varying qualifier is used for passing variables from a vertex shader
+ * to a fragment shader. In the fragment shader, these variables are
+ * interpolated between neighboring vertexes.
+ */
+varying vec3 vColor; // Color at a vertex
+
+uniform mat4 viewMat;
+uniform mat4 projectionMat;
+uniform mat4 modelViewMat;
+uniform mat3 normalMat;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+uniform Material material;
+
+uniform vec3 attenuation;
+
+uniform vec3 ambientLightColor;
+
+attribute vec3 position;
+attribute vec3 normal;
+
+
+/***
+ * NUM_POINT_LIGHTS is replaced to the number of point lights by the
+ * replaceNumLights() function in teapot.js before the shader is compiled.
+ */
+#if NUM_POINT_LIGHTS > 0
+
+	struct PointLight {
+		vec3 position;
+		vec3 color;
+	};
+
+	uniform PointLight pointLights[ NUM_POINT_LIGHTS ];
+
+#endif
+
+
+void main() {
+
+	// Compute ambient reflection
+	vec3 ambientReflection = material.ambient * ambientLightColor;
+
+	vColor = ambientReflection;
+
+	gl_Position =
+		projectionMat * modelViewMat * vec4( position, 1.0 );
+
+}
+` );
+
+var shaderNode = document.createElement( "script" );
+
+shaderNode.id = shaderID;
+
+shaderNode.setAttribute( "type", "x-shader/x-vertex" );
+
+shaderNode.appendChild( shader );
+
+document.body.appendChild( shaderNode );
